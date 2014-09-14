ce3e3234e4819e648ba85fd4f368ea6129c12bbe
Maxime Jeanmart <maxime.jeanmart@gmail.com>
2014-09-14 12:10:33 +0200

    cleanup the dataset_data variable as well

--- run_analysis.R
+++ run_analysis.R
@@ -149,5 +149,5 @@ dataset_tidy <- ddply(dataset_data, c("subjects", "activities"), function(data){
 write.table(dataset_data, file="dataset_tidy.txt", row.names=FALSE)
 
 ### cleanup code: remove all variables that are not the result of this project. In other words, only keep dataset_data and dataset_tidy
-remove(X_test, X_train, activityLabels, featureNames, full_test, full_train, merged, subject_test, subject_train, y_test, y_train, 
+remove(X_test, X_train, activityLabels, featureNames, full_test, full_train, merged, subject_test, subject_train, y_test, y_train, dataset_data,
        cols, colsBase, dataFileName, dataUri, filteredFeatureNames, i)
